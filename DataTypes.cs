f7030e0a9b66b4093ef3cae85f3d48b134b25d27
VinitKasture <97528616+VinitKasture@users.noreply.github.com>
2025-07-07 12:48:58 +0530

    Add files via upload

---
+++ DataTypes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLNotes
+{
+    public class DataTypes
+    {
+        /*
+         
+        CHAR
+        Char stores no of characters as per the length, if less characters are stored than
+        the length specified then the rest of the space is filled with spaces
+
+        Use CHAR when: All entries are the same length
+        
+        it stores CHAR as fixed-length, so it knows exactly how much space each value takes 
+        even on disk and in memory. That makes certain operations faster.
+
+
+        VARCHAR
+        It stores text strings with variable length
+
+        BINARY
+        it stores the data in bytes (binary data) rather than human readable text
+
+        VARBINARY
+        It is used to store raw binary data like images, files, encrypted values, 
+        or any custom byte data — but unlike BINARY, it only uses as much space as needed.
+
+        BIT
+        The BIT data type is used to store Boolean-like values — essentially, 0 (false) or 1 (true)
+        stores 1 if provided value greater than 1 as true
+
+        TINYINT
+        Only stores positive values
+
+        SMALLINT, INT, BIGINT
+        Can store both negative and positive values
+        {tinyint, smallint, int - each one have a different range}
+
+        FLOAT
+        A floating-point numeric data type used to store approximate numerical 
+        values with floating decimal points.
+
+        FLOAT and REAL store numbers using binary floating-point representation, not exact decimal.
+        This method trades accuracy for range and performance.
+        Because some decimal numbers can’t be represented exactly in binary, 
+        the value stored is very close, but not always exactly the same as what you input.
+         
+        DECIMAL
+        A fixed precision and scale numeric data type used to store exact 
+        numeric values with defined digits before and after the decimal point.
+
+        REAL
+        A floating-point numeric data type that stores approximate numeric values
+        with less precision than FLOAT.
+        
+        DATE
+        Only the date (year, month, day)
+
+        DATETIME
+        Both date and time
+
+        DATETIME2
+        Date and time (like DATETIME) but with more precision and a larger range
+
+        TIME
+        Only the time part, no date
+
+         
+
+         */
+    }
+}
---
+++ Methods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLNotes
+{
+    internal class Methods
+    {
+
+        /*
+ 
+ 
+ 
+
+        Fetch Offset
+        fetch offset allows us to fetch a certain part of the result of a search query
+        usually used for pagination. we can choose how many rows to skip and 
+        how many rows to return.
+
+        Inner Join
+        used to combine rows from two or more tables with one common column from both table,
+        it returns only the matching rows from both table.
+    
+        left / right join
+        --left
+        Returns all rows from the left table (Customers), and matching rows 
+        from the right table (Orders). If no match is found, NULLs are returned 
+        for the right table columns.
+       
+        FULL OUTER JOIN
+        returns all the rows from both table if the match is not found then null
+        are returned
+
+        GROUP BY
+        groups the data together based on a specific column along with an aggreate function
+        like sum, avg, count
+ 
+        HAVING
+        WHERE filters rows before grouping,
+        HAVING filters groups after grouping.
+
+        COUNT(), SUM(), AVG() --- NAHH, Bruh!
+
+        WITH
+        used to create a temperory mini table with your querries which you can reuse inside a query
+
+        ROW_NUMBER
+        returns a row number
+
+        CREATE VIEW
+        a virtual table, it doesnt store the data in it self but rather shows the data from 
+        the tables underneath
+
+        FOREIGN KEY
+        foreign key creates a relation between two tables, a primary key from one table used in
+        a child table. 
+        Ensures you can’t insert a value in the child table unless it exists in the parent table
+        Helps maintain referential integrity
+
+        DEFAULT
+        sets a default value for a column if isn't provided
+
+        UNIQUE
+        ensures no duplicate record in the column
+ 
+
+        */
+    }
+}
